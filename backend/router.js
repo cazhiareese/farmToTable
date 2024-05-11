4a34abc7682a80947d6ac22bd9b56c2a7ce3912e
cazhiareese <clleva@up.edu.ph>
2024-05-11 23:17:17 +0800

    feat: added homepage

--- backend/router.js
+++ backend/router.js
@@ -1,13 +1,16 @@
+import {getAllProducts} from './controller.js'
 
 
-export default router = (app) => {
+const router = (app) =>{
 
-       app.use((req, res, next) => {
-              res.setHeader('Access-Control-Allow-Origin', '*');
-              res.setHeader('Access-Control-Allow-Credentials', 'true');
-              res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-              res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-              next();
-       })
+    app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+        res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+        next();
+      })
+    app.get('/products', getAllProducts)
 }
 
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,12 @@
 import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-// import router from './router.js';
-
-const dbURI = 'mongodb+srv://lscombalicer2:C2ByZOUd72H1BNNN@farm.kzqurki.mongodb.net/ftt?retryWrites=true&w=majority&appName=farm';
+import router from './router.js';   // import the router function
+//Server configuration file
+//runs the application
 const app = express();
-app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
 
-//Connect to MongoDB & Initialize server
-try {
-       await mongoose.connect(dbURI);
-       app.listen(3001, () => {console.log("Server started at port 3000.")});
-} catch(error) {
-       console.log("Server initialization error");
-}
+router(app);
 
-
-
-// router(app);
+app.listen(3001);
