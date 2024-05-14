8d6ff1e4a9fb4135532cb4d72762470849afc611
Mairuzu1 <146474697+Mairuzu1@users.noreply.github.com>
2024-05-14 17:59:45 +0800

    feat: added userController.js

---
+++ backend/controller/userController.js
@@ -0,0 +1,14 @@
+import { User } from '../models/userSchema.js';
+
+// getUser
+export const getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
