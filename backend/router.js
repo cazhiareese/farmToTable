0744a051cc14fa8c6f353616f365512c4c8c2b3f
Mairuzu1 <146474697+Mairuzu1@users.noreply.github.com>
2024-05-14 18:01:11 +0800

    feat: Update router.js for getUser

--- backend/router.js
+++ backend/router.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { getProducts, getProductById, addOrder, removeOrder } from './controller/productController.js';
 import { registerUser, loginUser } from './controller/auth.js'; // Import auth functions here
+import { getUser } from './controller/userController.js';
 
 const router = express.Router();
 
@@ -13,5 +14,8 @@ router.delete('/orders/:id', removeOrder);
 // Auth routes
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+
+// User routes
+router.get('/users/:id', getUser); // Define the getUser route
 
 export default router;
