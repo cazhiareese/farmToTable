4ae1b95c2bb76b8a6e7b143b130bf1488fb62cc9
alyssa2603 <afcea@up.edu.ph>
2024-05-15 17:47:55 +0800

    chore: updated cart

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -1,57 +1,62 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
-function Cart (){
+function Cart() {
 
     let userId = useParams();
 
     const [pushCart, setCart] = useState([]);
     const [totalItems, setVal] = useState(0);
     const [prodDeets, setDetails] = useState([]);
-  
-    useEffect(() => {    
+
+    useEffect(() => {
         let url = `http://localhost:3001/getCart/${userId.id}`
-        console.log(userId);
-        console.log(typeof userId)
+
         fetch(url)
-          .then(response => response.json())
-          .then(body => {
-            setCart(body.cart)
-            console.log(body.cart)
-        })
+            .then(response => response.json())
+            .then(body => {
+                setCart(body.cart)
+                countItems(body.cart)
+                let productids = [...new Set(body.cart.map(item => item.productId))]
+                console.log(productids)
+                for (let j = 0; j < body.cart.length - 1; j++) {
+                    getProduct(productids[j])
+                }
+                console.log(body.cart)
+            })
 
-        
-    },[])
 
-    function handleCartChange(cart){
+    }, [])
+
+    function handleCartChange(cart) {
         fetch('http://localhost:3001/updateCart',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        //   add tayo dito ng authenticator token
-        },
-        body: JSON.stringify({ 
-                // authenticator dapat ang ipapasa ko ha
-                userId: userId.id,
-            cart : cart
-        })
-      }).then(response => response.text())
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                    //   add tayo dito ng authenticator token
+                },
+                body: JSON.stringify({
+                    // authenticator dapat ang ipapasa ko ha
+                    userId: userId.id,
+                    cart: cart
+                })
+            }).then(response => response.text())
             .then(body => {
                 console.log(body)
-      })
+            })
     }
 
-    function removeFromCart (id){
+    function removeFromCart(id) {
         let i = 0
         for (let j = 0; j < pushCart.length; j++) {
             if (pushCart[j].id === id) {
-                i = j              
+                i = j
                 break;
             }
         }
 
-        setCart((currentCart) =>{
+        setCart((currentCart) => {
             const updateCart = [...currentCart];
             updateCart.splice(i, 1)
             countItems(updateCart)
@@ -61,7 +66,7 @@ function Cart (){
     }
 
     // handles quantity depending on increment or decrement chosen by user
-    function handleQuantity(id, num){
+    function handleQuantity(id, num) {
         let i = 0
         for (let j = 0; j < pushCart.length; j++) {
             if (pushCart[j].id === id) {
@@ -70,10 +75,10 @@ function Cart (){
             }
         }
 
-        setCart((currentCart) =>{
+        setCart((currentCart) => {
             const updateCart = [...currentCart];
-            updateCart[i]  =  { ...updateCart[i], qty: updateCart[i].qty + num};
-            if (updateCart[i].qty === 0){
+            updateCart[i] = { ...updateCart[i], qty: updateCart[i].qty + num };
+            if (updateCart[i].qty === 0) {
                 removeFromCart(updateCart[i].id)
             }
             countItems(updateCart)
@@ -83,69 +88,69 @@ function Cart (){
     }
 
 
-    function countItems(cart){
+    function countItems(cart) {
         console.log(cart)
         let newTotal = 0;
-        cart.map((prod) =>{
+        cart.map((prod) => {
             newTotal += prod.qty;
         })
         setVal(newTotal)
     }
 
-    function getProduct(id){
+    function getProduct(id) {
         const productId = id
-        let url = 'http://localhost:3001/getProduct/'+productId
+        let url = 'http://localhost:3001/getProduct/' + productId
         fetch(url).then(response => response.json())
-        .then(body => {
-          setDetails((prodDeets) => 
-            {
-                const newDetails = [...prodDeets, {id: body._id, name: body.name, price: body.price}]
-                console.log(newDetails)
-                return newDetails
-            }
-          )
-          
-      })
+            .then(body => {
+                setDetails((prodDeets) => {
+                    const newDetails = [...prodDeets, { id: body._id, name: body.name, price: body.price }]
+                    console.log(newDetails)
+                    return newDetails
+                })
+            })
 
-      console.log(prodDeets)
+        console.log(prodDeets)
 
     }
 
-    return(
+    return (
         <div className='cart-page'>
             <h1>Shopping Cart (Total Items : {totalItems})</h1>
 
             <div className="cart-items">
+                {
+                    pushCart.map((prod) => {
+                        return (
+                            <div key={prod.id} className="added-product">
+                                {console.log("one item: " + prod.productId)}
 
-           
-            {
-            pushCart.map((prod)=> {
-                return(
-                    <div key={prod.id} className="added-product">
 
-                        {/* {getProduct(prod.productId)} */}
+                                {/* 
+                                {prodDeets.map((item) => {
+                                    if (prod.productId === item.id) {
+                                        return (
+                                            <>
+                                                <h1>{item.name}</h1>
+                                                <h3>{item.price}</h3>
+                                            </>
+                                        )
+                                    }
+                                })} */}
 
-                        {prodDeets.map(product => (
-                            <div key={product.id}>
-                            <h3>{product.name}</h3>
-                            <p>Price: {product.price}</p>
+                                <div className="cart-qty">
+                                    <button onClick={() => handleQuantity(prod.id, -1)}>-</button>
+                                    <p>Qty: {prod.qty} </p>
+
+                                    <button onClick={() => handleQuantity(prod.id, 1)}>+</button>
+                                </div>
+                                {/* Remove an item regardless of quantity */}
+                                <div className="remove">
+                                    <button onClick={() => removeFromCart(prod.id)}>Remove From Cart</button>
+                                </div>
                             </div>
-                        ))}
-                        {/* Handle quantity depending on button clicked by user */}
-                        <div className="cart-qty">
-                        <button onClick={()=>handleQuantity(prod.id,-1)}>-</button>
-                        <p>Qty: {prod.qty} </p>
-                
-                        <button onClick={()=> handleQuantity(prod.id,1)}>+</button>
-                        </div>
-                        {/* Remove an item regardless of quantity */}
-                        <div className="remove">
-                        <button onClick={() => removeFromCart(prod.id)}>Remove From Cart</button>
-                        </div>
-                    </div>
-                )
-            })
-            }
+                        )
+                    })
+                }
             </div>
         </div>
     )
