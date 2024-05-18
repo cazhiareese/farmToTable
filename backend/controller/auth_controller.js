33c11d6d33d70441dae8e67ae971cc96db546ec1
Mairuzu1 <johnmiles23.jmr@gmail.com>
2024-05-18 08:42:53 +0800

    chore: Updated user_controller.js with all the endpoints, Updated router.js and server.js

--- backend/controller/auth_controller.js
+++ backend/controller/auth_controller.js
@@ -2,7 +2,7 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { User } from '../models/userSchema.js';
 
-SECRET_KEY = 'CMSC100FTT';
+const SECRET_KEY = 'CMSC100FTT';
 
 //sign up authentication
 const signUp = async (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import router from './router.js';   // import the router function
-//Server configuration file
-//runs the application
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Use the router
 router(app);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Something went wrong' });
+  });
 
 app.listen(3001);
