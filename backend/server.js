98b9eb4d087fd504b9fab312f7570abc80102745
Mairuzu1 <johnmiles23.jmr@gmail.com>
2024-05-18 09:03:34 +0800

    chore: Updated user_controller.js with all the endpoints, Updated router.js and server.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,19 @@
 import express from 'express';
-import router from './router.js';   // import the router function
+import router from './router.js';   // import the router
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Use the router
-router(app);
+app.use(router);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: 'Something went wrong' });
-  });
+});
 
-app.listen(3001);
+app.listen(3001, () => {
+    console.log('Server is running on port 3001');
+});
